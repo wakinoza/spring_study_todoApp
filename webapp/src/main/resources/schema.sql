33242d4b39cbd37a06d0504eaa2e017671b09707
wakinoza <wakinoza@gmail.com>
2026-04-02 11:59:14 +0900

    DBテーブルの情報を記載

---
+++ webapp/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS todos;
+
+CREATE TABLE todos (
+	id serial PRIMARY KEY,
+	todo varchar(255) NOT NULL,
+	detail text,
+	created_at timestamp without time zone,
+	updated_at timestamp without time zone
+);
