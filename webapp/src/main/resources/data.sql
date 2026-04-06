7a84e1d4a89f5d603b1e28c0ac4e92921e0eac2d
wakinoza <wakinoza@gmail.com>
2026-04-06 13:48:41 +0900

    DBの初期値の記述

---
+++ webapp/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO todos (todo, detail, created_at, updated_at)
+VALUES ('買い物', 'スーパーで食材を購入する', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO todos (todo, detail, created_at, updated_at)
+VALUES ('図書館に行く', '本を借りる', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO todos (todo, detail, created_at, updated_at)
+VALUES ('ジムに行く', '運動する', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
