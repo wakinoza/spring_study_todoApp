557339a5388dde4041520debfb3eeb95c22ce656
wakinoza <wakinoza@gmail.com>
2026-04-06 14:05:40 +0900

    ToDoMapperクラスの記述

---
+++ webapp/src/main/java/com/example/webapp/repository/TodoMapper.java
@@ -0,0 +1,34 @@
+package com.example.webapp.repository;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import com.example.webapp.entity.ToDo;
+
+@Mapper
+public interface TodoMapper {
+  /**
+   * 全てのすることを取得します
+   */
+  List<ToDo> selectAll();
+
+  /**
+   * 指定されたIDに対応するすることを取得します
+   */
+  ToDo selectById(@Param("id") Integer id);
+
+  /**
+   * することを登録します
+   */
+  void insert(ToDo todo);
+
+  /**
+   * することを更新します
+   */
+  void update(ToDo todo);
+
+  /**
+   * 指定されたIDのすることを削除します
+   */
+  void delete(@Param("id") Integer id);
+}
