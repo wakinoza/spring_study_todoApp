4e7494a72a1c3f7260dc1f2529a20526504c8d25
wakinoza <wakinoza@gmail.com>
2026-04-08 16:28:38 +0900

    動作確認用のコードを記述

--- webapp/src/main/java/com/example/webapp/WebappApplication.java
+++ webapp/src/main/java/com/example/webapp/WebappApplication.java
@@ -3,7 +3,7 @@ package com.example.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.example.webapp.entity.ToDo;
-import com.example.webapp.repository.ToDoMapper;
+import com.example.webapp.service.ToDoService;
 import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
@@ -17,34 +17,34 @@ public class WebappApplication {
   /**
    * DI
    */
-  private final ToDoMapper mapper;
+  private final ToDoService service;
 
   public void exe() {
     System.out.println("====全件検索====");
-    for (ToDo row : mapper.selectAll()) {
+    for (ToDo row : service.findAllToDo()) {
       System.out.println(row);
     }
 
     System.out.println("====1件検索====");
-    System.out.println(mapper.selectById(1));
+    System.out.println(service.findByIdToDo(1));
 
     ToDo todo = new ToDo();
     todo.setTodo("リポジトリのテスト");
     todo.setDetail("DBへの登録処理");
-    mapper.insert(todo);
+    service.insertToDo(todo);
     System.out.println("====登録確認====");
-    System.out.println(mapper.selectById(4));
+    System.out.println(service.findByIdToDo(4));
 
-    ToDo target = mapper.selectById(4);
+    ToDo target = service.findByIdToDo(4);
     target.setTodo("リポジトリのテスト：更新");
     target.setDetail("DBへの更新処理");
-    mapper.update(target);
+    service.updateToDo(target);
     System.out.println("====更新確認====");
-    System.out.println(mapper.selectById(4));
+    System.out.println(service.findByIdToDo(4));
 
-    mapper.delete(4);
+    service.deleteToDo(4);
     System.out.println("====削除確認====");
-    for (ToDo row : mapper.selectAll()) {
+    for (ToDo row : service.findAllToDo()) {
       System.out.println(row);
     }
 
