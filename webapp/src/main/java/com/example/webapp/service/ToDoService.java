9ca2d3196a4f5a0c9bb93553218f927c68a91af0
wakinoza <wakinoza@gmail.com>
2026-04-08 16:01:20 +0900

    ToDoServiceインターフェースの記述

---
+++ webapp/src/main/java/com/example/webapp/service/ToDoService.java
@@ -0,0 +1,47 @@
+package com.example.webapp.service;
+
+import java.util.List;
+import com.example.webapp.entity.ToDo;
+
+/**
+ * ToDo サービス
+ */
+public interface ToDoService {
+  /**
+   * 全てのすることを検索します
+   * 
+   * @return
+   */
+  List<ToDo> findAttToDo();
+
+  /**
+   * 指定されたIDのすることを検索します
+   * 
+   * @param id ID
+   * @return ToDo
+   */
+  ToDo findByIdToDo(Integer id);
+
+  /**
+   * することを新規登録します
+   *
+   * @param toDo
+   */
+  void insertToDo(ToDo toDo);
+
+  /**
+   * することを更新します
+   * 
+   * @param toDo
+   */
+  void updateToDo(ToDo toDo);
+
+  /**
+   * 指定されたIDnoすることを削除します
+   *
+   * @param id
+   */
+  void deleteToDo(Integer id);
+
+
+}
