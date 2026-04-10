52bc6f9f85c06a945c738d0f664e38c5d1a573c9
wakinoza <wakinoza@gmail.com>
2026-04-10 14:20:14 +0900

    ToDoクラスとTodoFormクラス間の変換を行うクラスの作成

---
+++ webapp/src/main/java/com/example/webapp/helper/ToDoHelper.java
@@ -0,0 +1,34 @@
+package com.example.webapp.helper;
+
+import com.example.webapp.entity.ToDo;
+import com.example.webapp.form.ToDoForm;
+
+/**
+ * ToDo:ヘルパー
+ */
+public class ToDoHelper {
+  /**
+   * ToDoへの変換
+   */
+  public static ToDo convertToDo(ToDoForm form) {
+    ToDo todo = new ToDo();
+    todo.setId(form.getId());
+    todo.setTodo(form.getTodo());
+    todo.setDetail(form.getDetail());
+    return todo;
+  }
+
+  /**
+   * ToDoFormへの変換
+   */
+  public static ToDoForm convertToDoForm(ToDo todo) {
+    ToDoForm form = new ToDoForm();
+    form.setId(todo.getId());
+    form.setTodo(todo.getTodo());
+    form.setDetail(todo.getDetail());
+    form.setIsNew(false);
+    return form;
+
+  }
+
+}
